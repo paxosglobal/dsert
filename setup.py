94905037c83ab79fdcdfef05624992a8ba91c8ed
Peter Grace <pete@stackoverflow.com>
2017-01-26 21:02:58 +0000

    use setuptools instead of distutils.core; find_packages() to get actual modules to install; dynamic version number; add github url to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
-from distutils.core import setup
+from setuptools import (
+    setup,
+    find_packages
+)
+from datetime import datetime
+
+version = "0.1.0+{}".format(datetime.now().strftime("%Y%m%d.%H%M%S"))
 
 setup(
     name='dsert',
-    version='0.1.0',
-    url='',
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    url='https://github.com/paxos-bankchain/dsert',
     license='',
     author='paxosdev',
     author_email='',
