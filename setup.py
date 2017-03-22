82946946476e1514e72905d2bdffced4f42532b0
Michael Flaxman <michael.flaxman@gmail.com>
2017-03-22 17:52:58 -0400

    pypi (#8)

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsert',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     url='https://github.com/paxos-bankchain/dsert',
-    license='',
-    author='paxosdev',
-    author_email='',
-    description='',
+    license='MIT',
+    author='Paxos Trust Company, LLC',
+    author_email='pypi@paxos.com',
+    description='Test all fields of a python dictionary',
+    classifiers=[
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
