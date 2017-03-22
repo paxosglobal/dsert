aef9cdbae74189cdb38a15af0f86dbda154318bb
Michael Flaxman <mflaxman@itbit.com>
2017-03-22 18:19:09 -0400

    pypi tweaks

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsert',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     url='https://github.com/paxos-bankchain/dsert',
     license='MIT',
